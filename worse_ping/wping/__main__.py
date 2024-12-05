5c04e7b6147ab2c35d636284269fcfe980fd777a
mynorjrv <mynorjrv@gmail.com>
2024-12-05 10:08:19 -0600

    A really bad bad bad ping

--- worse_ping/wping/__main__.py
+++ worse_ping/wping/__main__.py
@@ -1,4 +1,8 @@
 from .wping import main
 
 # https://realpython.com/python-typer-cli/
+# https://medium.com/@noransaber685/simple-guide-to-creating-a-command-line-interface-cli-in-python-c2de7b8f5e05
+# https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html
+# https://github.com/pypa/setuptools
+# https://github.com/fastapi/typer/tree/master/typer
 main()
