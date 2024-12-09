7e84ce99b31cbc4717fd4157f47fc36321146625
mynorjrv <mynorjrv@gmail.com>
2024-12-09 16:28:48 -0600

    Not much, but rethinking the vehicle class

--- labs/4.1.1-encoder-decoder.py
+++ labs/4.1.1-encoder-decoder.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 import json
 
-@dataclass
 class Vehicle:
-    regitration_number: str
-    year_of_production: int
-    passenger: bool
-    mass: float
-
+    def __init__(
+        self,
+        regitration_number: str,
+        year_of_production: int,
+        passenger: bool,
+        mass: float
+    ):
+        ...
+        
+    
+Vehicle(1, 123213, False, 1.251212)
