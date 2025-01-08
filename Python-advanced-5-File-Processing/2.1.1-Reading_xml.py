49c9ca1b89ff9fc52b1dea5e0f63e965cecb66d4
mynorjrv <mynorjrv@gmail.com>
2025-01-08 15:33:26 -0600

    Working again with xml

--- Python-advanced-5-File-Processing/2.1.1-Reading_xml.py
+++ Python-advanced-5-File-Processing/2.1.1-Reading_xml.py
@@ -8,3 +8,29 @@ print('The root tag is:', root.tag)
 print('The root has the following children:')
 for child in root:
     print(child.tag, child.attrib)
+
+
+# Accessing tree elements by index
+print("My books:\n")
+for book in root:
+    print('Title: ', book.attrib['title'])
+    print('Author:', book[0].text)
+    print('Year: ', book[1].text, '\n')
+
+
+# Using iter method, all elements with tag
+for author in root.iter('author'):
+    print(author.text)
+
+
+# Retriving all childrens
+for book in root.findall('book'):
+    print(book.get('title'))
+# Dont know how this line work
+for author in root.findall('author'):
+    print(author.text)
+print()
+
+
+# find returns the first element
+print(root.find('book').get('title'))
